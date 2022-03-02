89ab28ae632c9355b08b00aa0875aba73c8b7e23
rvkuk4 <rvkuk4@gmail.com>
2022-03-02 12:23:05 +0000

    added Sample project to SecondRepo

---
+++ sample/src/sample/basic/basic.java
@@ -0,0 +1,10 @@
+package sample.basic;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("basic case");
+	}
+
+}
---
+++ sample/src/sample/sample.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class sample {
+	static int x = 10;
+	String y = "hello" ;
+
+
+public static void main() {
+	System.out.print(x);
+}
+}
